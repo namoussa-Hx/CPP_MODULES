d8f7ef34dbc724f0c2a4af17e16195b23a6d250d
najib3762 <najibamoussaoui5@gmail.com>
2024-10-08 14:53:43 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main (int ac, char **av)
+{
+    if(ac > 1)
+    {
+    for(int i = 1; i < ac; i++)
+    {
+        std::string str = av[i];
+        for(int j = 0; j < (int)str.length(); j++)
+        {
+            char c = str[j];
+            std::cout << (char )std::toupper(c);
+        }
+    std::cout << " ";
+    }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+ return 0;
+}
