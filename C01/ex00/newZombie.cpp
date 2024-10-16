1bff894f5a7b8d710de33d09af451eaa9f264af8
najib3762 <najibamoussaoui5@gmail.com>
2024-10-16 18:51:06 +0100

    change_ex01

--- C01/ex00/newZombie.cpp
+++ C01/ex00/newZombie.cpp
@@ -1,6 +1,5 @@
 #include "Zombie.hpp"
 
-
 Zombie* newZombie( std::string name)
 {
     Zombie *ptr = new Zombie(name);
--- C01/ex00/randomChump.cpp
+++ C01/ex00/randomChump.cpp
@@ -2,6 +2,6 @@
 
 void randomChump(std::string name)
 {
-    Zombie *ptr = newZombie(name);
-    ptr->announce();
+    Zombie ptr(name);
+    ptr.announce();
 }
