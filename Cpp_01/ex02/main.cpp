5b95cd3fe09c4a8c40e4a63de32bfafff5bc9702
Mohamed Amoussaoui <mohamedamoussaoui20@gmail.com>
2024-10-17 01:00:05 +0100

    value_address

--- Cpp_01/ex02/main.cpp
+++ Cpp_01/ex02/main.cpp
@@ -5,14 +5,15 @@ int main ()
 {
     std::string ptr = "HI THIS IS BRAIN";
     std::string *stringPTR = &ptr;
-    std::string& stringREF = *stringPTR;
+    std::string& stringREF = ptr;
 
-    std::cout << &ptr << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl;
-    std::cout << stringREF << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << ptr << std::endl;
+    std::cout << "The memory address of the string variable: " << &ptr << std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "The value of the string variable: " << stringREF << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << ptr << std::endl;
 
 
     return 0;
