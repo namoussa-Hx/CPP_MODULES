871b1e46cecfbf3a332174c3f42c2e586a1381a3
najib3762 <najibamoussaoui5@gmail.com>
2024-10-19 19:47:04 +0100

    newpushc01

--- Cpp_01/ex04/main.cpp
+++ Cpp_01/ex04/main.cpp
@@ -2,10 +2,11 @@
 
 int main (int ac, char **av)
 {
-    if(ac != 4)
+    if(ac != 4 || ac == 1)
     {
         std::cout << "Error: invalide number of argumants " 
         << std::endl;
+	return (1);
     }
     exec(av[1], av[2], av[3]);
    return (0);
