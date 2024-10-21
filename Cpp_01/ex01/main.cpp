528f2dc706a8b2947ac854d984cb449cd562510d
Mohamed Amoussaoui <mohamedamoussaoui20@gmail.com>
2024-10-21 14:32:35 +0100

    new_push

--- Cpp_01/ex01/main.cpp
+++ Cpp_01/ex01/main.cpp
@@ -2,9 +2,11 @@
 
 int main ()
 {
-    int n = 6;
-    Zombie *ptr = zombieHorde(n, "Zombie");
+    int n = -1;
 
+    Zombie *ptr = zombieHorde(n, "Zombie");
+    if(!ptr)
+       return 1;
     for(int i = 0; i < n; i++)
     {
         ptr[i].announce();
--- Cpp_01/ex01/zombieHorde.cpp
+++ Cpp_01/ex01/zombieHorde.cpp
@@ -3,8 +3,9 @@
 
 Zombie* zombieHorde(int n, std::string name)
 {
+    if(n < 0)
+       return NULL;
     Zombie *arr = new Zombie[n];
-
     for(int i = 0; i < n; i++)
     {
         arr[i].set_name(name);
